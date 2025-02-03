3b8662fb44443a06b3bb5be432ea42040c071bda
epavlyuk <epavlyuk@elvees.com>
2025-02-03 18:41:35 +0300

    added a part of vector methods

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include "vector/include/vector.h"
-
 #include <iostream>
 
 int main()
 {
+    std::cout << "creating vector vec_1" << std::endl;
     Vector<int> vec_1;
 
+    std::cout << "vec_1.push_back()" << std::endl;
+    vec_1.push_back(1);
+
+    
     return 0;
 }
--- vector/include/vector.h
+++ vector/include/vector.h
@@ -1,8 +1,33 @@
+#include <cstddef>
+
+#ifndef _VECTOR_
+#define _VECTOR_
 template<typename T>
 class Vector
 {
-    public:
-    
-    private:
+public:
+    Vector() = default;
+    Vector(const Vector<T>& other);
+    ~Vector();
 
+    void reserve(const std::size_t new_capacity);
+    void push_back(const T& value);
+    std::size_t size() const;
+    bool empty() const;
+    void pop_back();
+    void resize(const std::size_t count);
+
+    // оператор присваивания
+    // оператор индексирования [] + const []
+    // merge
+    // итераторы
+
+private:
+    T * m_arr;
+    std::size_t m_size = 0;
+    std::size_t m_capacity = 0;
 };
+
+
+
+#endif // _VECTOR_
